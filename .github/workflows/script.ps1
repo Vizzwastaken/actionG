7c01e8197ca12893d59e7bdbdd52949f89876179
Vizzwastaken <91386217+Vizzwastaken@users.noreply.github.com>
2022-03-17 11:39:27 +0530

    Update script.ps1

--- .github/workflows/script.ps1
+++ .github/workflows/script.ps1
@@ -1,3 +1,3 @@
-new-item .\test.txt
-Compress-Archive -LiteralPath .\test.txt -DestinationPath .\archive\test.zip
+new-item D:\a\actionG\actionG\test.txt
+Compress-Archive -LiteralPath D:\a\actionG\actionG\test.txt -DestinationPath D:\a\actionG\actionGtest.zip
 ls 
